76c77e7a82705f13ff6df2e3263521045cad9acb
Pedro Hamms Vietro <hammsvietro@gmail.com>
2020-07-09 18:30:10 -0300

    change database dialect to sqlite

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,14 +5,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  client: process.env.DB_DIALECT,
+  client: 'sqlite3',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_NAME
+    filename: './src/database/db.sqlite'
   },
   migrations: {
     directory: './src/database/migrations'
-  }
+  },
+  useNullAsDefault: true,
 }
